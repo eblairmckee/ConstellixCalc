0fdd0a3aee8772499e792bc21f87060caf3524ea
NobleNerd <jr.noblenerd@gmail.com>
2017-01-10 11:15:15 -0500

    initial commit of Calc

---
+++ Calc1.js
@@ -0,0 +1,48 @@
+var domainTotal = 0;
+var recordTotal = 0;
+var gtdTotal = 0;
+
+// Domain Calculation
+function domainNumber(num) {
+    if (num > 0 && num < 2){
+       domainTotal = 5; 
+    } else if (num >= 2 && num < 26) {
+       var oneDomain = num - 1;
+       var total = oneDomain * .5 + 5;
+       domainTotal = total;
+
+    } else if (num > 25){
+        var oneDomain = num - 1;
+        var twentyfiveDomain = oneDomain - 24;
+        var total = twentyfiveDomain * .1 + 17;
+        domainTotal = total;
+    } else 
+       alert("Please enter number of domains")
+};
+
+// Record Calculation
+function recordNumber(num) {
+    if (num > 100) {
+        var newNum = num - 100;
+        var total = newNum * .5;
+        recordTotal = total;
+    }  else
+        recordTotal = 0;
+};
+
+//GTD Calculation
+function gtdNumber(num) {
+    if (num > 0 && num < 1) {
+        var gtdTotal = 5; 
+    } else if (num > 1 && num < 100) {
+        var newNum = num - 1;
+        var total = newNum * 1
+    }
+};
+
+// Total Sum
+function totalCost (){
+    var costTotal = (domainTotal + recordTotal + gtdTotal).toFixed(2);
+    console.log("Total: $" + costTotal);
+};
+
